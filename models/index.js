15679207c00978ac359a94a85fe32ea977d0ceac
EC2 Default User <ec2-user@ip-172-31-25-233.us-east-2.compute.internal>
2019-11-28 05:58:21 +0000

    useNewUrlParser: true added

--- models/index.js
+++ models/index.js
@@ -4,7 +4,7 @@ console.log(process.env.DATABASEURL);
 //mongoose.connect("mongodb://localhost/todos_api", { useNewUrlParser: true });
 var url = process.env.DATABASEURL || "mongodb://localhost:27017/todo_api"
 //mongoose.connect("mongodb://localhost:27017/todo_api", { useNewUrlParser: true});
-mongoose.connect(url)
+mongoose.connect(url, { useNewUrlParser: true})
 .then(function(){
     console.log("connected to database: " + url);
     console.log(process.env.DATABASEURL);
